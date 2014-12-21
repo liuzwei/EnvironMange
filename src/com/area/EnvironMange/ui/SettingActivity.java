91b7b9f2d8d617a2678a7bf774264e84f928eb4e
long <826321978@qq.com>
2014-12-21 21:32:22 +0800

    张海龙1210

--- src/com/area/EnvironMange/ui/SettingActivity.java
+++ src/com/area/EnvironMange/ui/SettingActivity.java
@@ -1,13 +1,21 @@
 package com.area.EnvironMange.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.area.EnvironMange.R;
 import com.area.EnvironMange.base.ActivityTack;
 import com.area.EnvironMange.base.BaseActivity;
+import com.umeng.update.UmengUpdateAgent;
+import com.umeng.update.UmengUpdateListener;
+import com.umeng.update.UpdateResponse;
+import com.umeng.update.UpdateStatus;
 
 /**
  * author: ${zhanghailong}
@@ -19,6 +27,9 @@ public class SettingActivity extends BaseActivity implements View.OnClickListene
     private ImageView back;
     private TextView setting_exit;
     private TextView updatePass;
+    private LinearLayout setting_check;//检查更新
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +46,8 @@ public class SettingActivity extends BaseActivity implements View.OnClickListene
 
         updatePass = (TextView) this.findViewById(R.id.set_update_pass);
         updatePass.setOnClickListener(this);
+        setting_check = (LinearLayout) this.findViewById(R.id.setting_check);
+        setting_check.setOnClickListener(this);
     }
 
     @Override
@@ -49,6 +62,34 @@ public class SettingActivity extends BaseActivity implements View.OnClickListene
                 break;
             case R.id.set_update_pass:
                 startActivity(new Intent(SettingActivity.this, UpdatePassActivity.class));
+                break;
+            case R.id.setting_check:
+                //检查更新
+                Resources res = getBaseContext().getResources();
+                String message = res.getString(R.string.check_new_version).toString();
+                progressDialog = new ProgressDialog(SettingActivity.this);
+                progressDialog.setMessage(message);
+                progressDialog.show();
+
+                UmengUpdateAgent.forceUpdate(mContext);
+
+                UmengUpdateAgent.setUpdateListener(new UmengUpdateListener() {
+                    @Override
+                    public void onUpdateReturned(int i, UpdateResponse updateResponse) {
+                        progressDialog.dismiss();
+                        switch (i){
+                            case UpdateStatus.Yes:
+                                Toast.makeText(mContext, "有新版本发现", Toast.LENGTH_SHORT).show();
+                                break;
+                            case UpdateStatus.No:
+                                Toast.makeText(mContext, "已是最新版本", Toast.LENGTH_SHORT).show();
+                                break;
+                            case UpdateStatus.Timeout:
+                                Toast.makeText(mContext, "连接超时", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                    }
+                });
                 break;
         }
     }
